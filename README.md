ba29a7b13dde933444e77ba64bd911cb719af56e
Anton Mazhurin <mazhurin@gmail.com>
2015-09-27 11:23:04 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# courseraDataCleaning
+
+Coursera Data Cleaning course project
+=======
+
+Course project assignment for "Getting and Cleaning Data" course on www.Coursera.org
+-------
+
+## Installation
+
+* Install R 
+
+* Get the data archive from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+* Unzip the data archive into the current folder. You should have the folder named "UCI HAR Dataset" in the current folder.
+
+
+## Example of usage
+
+* Open R, and load the run the script:
+ 
+> source("./run_analysis.R")
+
+* A dataframe HumanActivitySummary will be created
+
+> HumanActivitySummary[1:5, 1:4]
+Source: local data frame [5 x 4]
+Groups: Activity [1]
+
+  Activity Subject tBodyAcc-mean()-X tBodyAcc-mean()-Y
+     (chr)   (int)             (dbl)             (dbl)
+1   LAYING       1         0.2215982       -0.04051395
+2   LAYING       2         0.2813734       -0.01815874
+3   LAYING       3         0.2755169       -0.01895568
+4   LAYING       4         0.2635592       -0.01500318
+5   LAYING       5         0.2783343       -0.01830421
+
+
+* The dataframe will be stored in HumanActivitySummary.txt
+
+
+## Description
+
+The input dataset for this project is "Human Activity Recognition Using Smartphones Dataset" from 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+The input data set contains sensor signals from accelerometers and gyroscopes recorded during an experiment 
+with a group of 30 volunteers.
+Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) 
+wearing a smartphone on the waist.
+
+The run_analysis.R programm does the following: 
+1. Merges train and test datasets.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. Creates a tidy data set with the average of each variable for each activity and each subject.
+
+
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 ## Load all the data from the files
 
 features <- read.delim("./UCI HAR Dataset/features.txt", header = FALSE, sep = "", colClasses = "character")
@@ -6,7 +8,7 @@ subject_train <- read.delim("./UCI HAR Dataset/train/subject_train.txt", header 
 
 subject_test <- read.delim("./UCI HAR Dataset/test/subject_test.txt", header = FALSE, sep = "")
 
-labels <- read.delim("./UCI HAR Dataset/activity_labels.txt", header = FALSE, sep = "", colClasses = "character")
+activity_labels <- read.delim("./UCI HAR Dataset/activity_labels.txt", header = FALSE, sep = "", colClasses = "character")
 
 X_train <- read.delim("./UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "")
 
@@ -19,16 +21,16 @@ y_test <- read.delim("./UCI HAR Dataset/test/y_test.txt", header = FALSE, sep = 
 ## 1. Merges the training and the test sets to create one data set.
 
 ## merge X with Y for train
-train <- cbind(X_train, y_train)
+train <- cbind(y_train, X_train)
 
 ## merge train with subject
-train <- cbind(train, subject_train)
+train <- cbind(subject_train, train)
 
 ## merge X with Y for test
-test <- cbind(X_test, y_test)
+test <- cbind(y_test, X_test)
 
 ## merge test with subject
-test <- cbind(test, subject_test)
+test <- cbind(subject_test, test)
 
 ## merge train and test
 data_set <- rbind(train, test)
@@ -41,27 +43,31 @@ feature_indexes <- grep("mean()|std()", features[,2])
 # create column names vector
 column_names <- features[feature_indexes, 2]
 
-# add label column index, which is 1
-feature_indexes <- c(feature_indexes, nrow(features)+1, nrow(features)+2)
+# add subject and activity column indexes
+feature_indexes <- feature_indexes + 2
+feature_indexes <- c(1, 2, feature_indexes)
 
 # choose the features we need
 data_set <- data_set[, feature_indexes]
 
+## 3. Uses descriptive activity names to name the activities in the data set
+for(i in 1: nrow(data_set)){
+  data_set[i,2] <- activity_labels[data_set[i,2],2]
+}
+
 ## 4. Appropriately labels the data set with descriptive variable names. 
 
 # add activity and subject column names
-column_names <- c(column_names, "Activity", "Subject")
+column_names <- c("Subject", "Activity", column_names)
 
 # set new column names
 colnames(data_set) <- column_names
 
-## 3. Uses descriptive activity names to name the activities in the data set
-for(i in 1: length(data_set)){
-  data_set$Activity[i] <- labels[i,2]
-}
-
-## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## 5. From the data set in step 4, creates a second, independent tidy data set
+## with the average of each variable for each activity and each subject.
 
 
+HumanActivitySummary <- data_set %>% group_by(Activity, Subject) %>% summarise_each(funs(mean)) 
 
-
+# Save the resulting data in a file
+write.table(HumanActivitySummary, "./HumanActivitySummary.txt", row.names = FALSE)
